06fe7d648262de77b3eebd21b5fc33ad75a70779
Saran Kumar Ramesh <ramesh0192@saskpolytech.ca>
2024-04-01 02:09:51 -0600

    🛠️ Worked on Home component and implemented routing for easy navigation
    - Worked on Home component HTML, CSS, and a bit of TypeScript to implement all necessary functions for the landing page
    - Implemented routing in all components for easy navigation, involving changes in app-routing.module.ts, app.component.html, and app.component.ts
    - Added navigation links and connected them with all respective components, resulting in changes in CSS and TypeScript files across all components

--- my-website/src/app/app-routing.module.ts
+++ my-website/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillPrjComponent } from './skill-prj/skill-prj.component';
+import { ExpEduComponent } from './exp-edu/exp-edu.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: SkillPrjComponent },
+  { path: 'experience', component: ExpEduComponent },
+  { path: 'contact', component: ContactMeComponent },
+  // Add more routes as needed
+  { path: '**', redirectTo: '' } // Redirect invalid routes to the home page
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- my-website/src/app/contact-me/contact-me.component.ts
+++ my-website/src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-me',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class ContactMeComponent {
 
+  constructor(private router: Router) { }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    // Get the height of the document, including any overflow content
+    const documentHeight = document.documentElement.scrollHeight;
+
+    // Get the height of the viewport
+    const viewportHeight = window.innerHeight;
+
+    // Get the current scroll position
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(scrollPosition);
+
+    // Calculate the position at the end of the Home component
+    const endOfHomePosition = documentHeight - viewportHeight;
+
+    const upThreshold = 0.10 * endOfHomePosition;
+    console.log(scrollPosition);
+
+    if (scrollPosition <= upThreshold) {
+      this.router.navigateByUrl('/experience'); // Navigate back to the About component
+    }
+  }
 }
+
+
